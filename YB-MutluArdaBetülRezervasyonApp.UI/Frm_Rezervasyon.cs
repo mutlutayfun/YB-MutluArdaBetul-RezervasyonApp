ce1f04fe56774cfa2622e84042bd01c68d5a30c4
Arda Can Bodak <elsharwy06@gmail.com>
2024-07-22 21:32:39 +0300

    Taslak Frm_Rezervasyon eklendi.

---
+++ YB-MutluArdaBetülRezervasyonApp.UI/Frm_Rezervasyon.cs
@@ -0,0 +1,33 @@
+namespace YB_MutluArdaBetülRezervasyonApp.UI
+{
+    public partial class Frm_Rezervasyon : Form
+    {
+        public Frm_Rezervasyon()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnExit_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show(" Mutlu Günler... ");
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
