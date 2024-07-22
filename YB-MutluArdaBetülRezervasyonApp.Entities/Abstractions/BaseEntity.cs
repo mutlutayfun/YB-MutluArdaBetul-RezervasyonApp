5048fa4e13f1ab0d7a375c738034af3817c5dad2
Mutlu Tayfun <mutlu.tayfunn@gmail.com>
2024-07-22 12:12:33 +0300

    BaseEntity Eklendi.

---
+++ YB-MutluArdaBetülRezervasyonApp.Entities/Abstractions/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YB_MutluArdaBetülRezervasyonApp.Entities.Abstractions
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
