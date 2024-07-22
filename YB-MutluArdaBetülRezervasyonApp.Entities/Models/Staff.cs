18013aeeca35cd987523309870e0f30bbc529aef
betül aktoprak <betulaktoprak112@gmail.com>
2024-07-22 12:22:12 +0300

    Hotel ve Staff sınıfları eklendi

--- YB-MutluArdaBetülRezervasyonApp.Entities/Models/Staff.cs
+++ YB-MutluArdaBetülRezervasyonApp.Entities/Models/Staff.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YB_MutluArdaBetülRezervasyonApp.Entities.Abstractions;
 
 namespace YB_MutluArdaBetülRezervasyonApp.Entities.Models
 {
-    public class Staff
+    public class Staff : BaseEntity
     {
+        public int HotelID { get; set; }
+        public Hotel Hotel { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public byte Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime HireDate { get; set; }
     }
 }
