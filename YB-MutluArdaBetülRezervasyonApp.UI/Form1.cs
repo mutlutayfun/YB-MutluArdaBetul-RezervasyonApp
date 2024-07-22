b327c346255edcdd8b3cc93f0a6126473b6cdf76
Mutlu Tayfun <mutlu.tayfunn@gmail.com>
2024-07-22 11:24:05 +0300

    Add project files.

---
+++ YB-MutluArdaBetülRezervasyonApp.UI/Form1.cs
@@ -0,0 +1,10 @@
+namespace YB_MutluArdaBetülRezervasyonApp.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
