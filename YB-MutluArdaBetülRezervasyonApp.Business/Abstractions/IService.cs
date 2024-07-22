3e847654e9b51614a730318f754683c205cf1b9e
Arda Can Bodak <elsharwy06@gmail.com>
2024-07-22 19:36:17 +0300

    IService ve IGuestService eklendi

---
+++ YB-MutluArdaBetülRezervasyonApp.Business/Abstractions/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB_MutluArdaBetülRezervasyonApp.Entities.Abstractions;
+
+namespace YB_MutluArdaBetülRezervasyonApp.Business.Abstractions
+{
+    public interface IService<T> where T : BaseEntity
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(Guid id);
+        IEnumerable<T>? GetAll();
+        T? GetByID(Guid id);
+    }
+}
---
+++ YB-MutluArdaBetülRezervasyonApp.Business/Services/IGuestService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB_MutluArdaBetülRezervasyonApp.Business.Abstractions;
+using YB_MutluArdaBetülRezervasyonApp.Business.Validators;
+using YB_MutluArdaBetülRezervasyonApp.DataAccessLayer.Repositories;
+using YB_MutluArdaBetülRezervasyonApp.Entities.Models;
+using FluentValidation.Results;
+
+namespace YB_MutluArdaBetülRezervasyonApp.Business.Services
+{
+    public class IGuestService : IService<Guest>
+    {
+        private readonly IGuestRepository _guestRepository;
+
+        public IGuestService(IGuestRepository gRepo)
+        {
+            _guestRepository = gRepo;
+        }
+
+        public void Add(Guest entity)
+        {
+            GuestValidator gVal = new();
+            ValidationResult result = gVal.Validate(entity);
+            if (!result.IsValid)
+            {
+                throw new Exception(string.Join("\n", result.Errors));
+            }
+            _guestRepository.Add(entity);
+        }
+
+        public void Delete(Guid id)
+        {
+            var guest = _guestRepository.GetByID(id);
+
+            if (guest.IsActive)
+            {
+                throw new Exception("Aktif misafir silinemez!!");
+            }
+            _guestRepository.Delete(id);
+        }
+
+        public IEnumerable<Guest>? GetAll()
+        {
+            return _guestRepository.GetAll();
+        }
+
+        public Guest? GetByID(Guid id)
+        {
+            return _guestRepository.GetByID(id);
+        }
+
+        public void Update(Guest entity)
+        {
+            GuestValidator gVal = new();
+            ValidationResult result = gVal.Validate(entity);
+
+            if (result.IsValid)
+            {
+                _guestRepository.Update(entity);
+            }
+        }
+    }
+}
