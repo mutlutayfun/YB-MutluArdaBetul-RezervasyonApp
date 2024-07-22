3c14d5cbfa7573151ec334ce4d43b099aa5906b8
Arda Can Bodak <elsharwy06@gmail.com>
2024-07-22 12:20:11 +0300

    Guest , Booking ve RoomType clasları eklenip BaseEntity den miras alındı.

--- YB-MutluArdaBetülRezervasyonApp.Entities/Models/Booking.cs
+++ YB-MutluArdaBetülRezervasyonApp.Entities/Models/Booking.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YB_MutluArdaBetülRezervasyonApp.Entities.Abstractions;
 
 namespace YB_MutluArdaBetülRezervasyonApp.Entities.Models
 {
-    public class Booking
+    public class Booking : BaseEntity
     {
+        public string? RoomNumber { get; set; }
+        public DateTime CheckinDate { get; set; }
+        public DateTime CheckoutDate { get; set; }
+        public decimal? TotalPrice { get; set; }
+        public Guid GuestId { get; set; }
     }
 }
