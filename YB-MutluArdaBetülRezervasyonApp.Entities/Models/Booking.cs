7f1002e658701d113d87f839bca7087d5e640c80
betül aktoprak <betulaktoprak112@gmail.com>
2024-07-22 11:56:56 +0300

    Booking classı eklendi

---
+++ YB-MutluArdaBetülRezervasyonApp.Entities/Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YB_MutluArdaBetülRezervasyonApp.Entities.Models
+{
+    public class Booking
+    {
+    }
+}
